11f79d7ebf6441f96d755ce971a3b89d63e7fd28
dmtnndxr <dmitone@yandex.ru>
2026-05-11 15:46:50 +0300

    install tw

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
+import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/squat-tracker/" : "/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   test: {
     environment: "jsdom",
     globals: true,
